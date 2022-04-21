a9b483001d77f92d02763189082a93873e3444bf
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-04-21 11:52:24 +0200

    test expanded

--- Assignment1/App_Test.py
+++ Assignment1/App_Test.py
@@ -8,9 +8,13 @@ class TestBooleanQuery(unittest.TestCase):
     test_dictionary = make_dic_postings(path)
     test_q1 = test_dictionary['baby']
     test_q2 = test_dictionary['mother']
+    inters = Intersection(test_q1, test_q2)
 
-    def test_boolean(self):
-        inters = Intersection(self.test_q1, self.test_q2)
-        self.assertEqual(inters.intersect_and(), ['BleakHouseFullBookSummarySparkNotes.txt', 'Midnight’sChildrenFullBookSummarySparkNotes.txt'])
-        self.assertEqual(inters.intersect_or(), {'AllQuietontheWesternFrontFullBookSummarySparkNotes.txt', 'EthanFromeFullBookSummarySparkNotes.txt', 'HarryPotterandTheOrderofthePhoenixFullBookSummarySparkNotes.txt', 'HarryPotterandtheHalfBloodPrinceFullBookSummarySparkNotes.txt', 'TheAmericanFullBookSummarySparkNotes.txt', 'HarryPotterandtheSorcerer’sStoneFullBookSummarySparkNotes.txt', 'BleakHouseFullBookSummarySparkNotes.txt', 'HarryPotterandtheGobletofFireFullBookSummarySparkNotes.txt', 'OneFlewOvertheCuckoo’sNestFullBookSummarySparkNotes.txt', 'TheKitchenGodsWifeFullBookSummarySparkNotes.txt', 'BridesheadRevisitedFullBookSummarySparkNotes.txt', 'DivergentFullBookSummarySparkNotes.txt', 'DuneFullBookSummarySparkNotes.txt', 'Midnight’sChildrenFullBookSummarySparkNotes.txt', 'HarryPotterandtheDeathlyHallowsFullBookSummarySparkNotes.txt', 'DiaryofaWimpyKidFullBookSummarySparkNotes.txt', 'FlowersforAlgernonFullBookSummarySparkNotes.txt'})
-        self.assertEqual(inters.intersect_not(), {'HarryPotterandtheSorcerer’sStoneFullBookSummarySparkNotes.txt'})
+    def test_boolean_and(self):
+        self.assertEqual(self.inters.intersect_and(), ['BleakHouseFullBookSummarySparkNotes.txt', 'Midnight’sChildrenFullBookSummarySparkNotes.txt'])
+
+    def test_boolean_or(self):
+        self.assertEqual(self.inters.intersect_or(), {'AllQuietontheWesternFrontFullBookSummarySparkNotes.txt', 'EthanFromeFullBookSummarySparkNotes.txt', 'HarryPotterandTheOrderofthePhoenixFullBookSummarySparkNotes.txt', 'HarryPotterandtheHalfBloodPrinceFullBookSummarySparkNotes.txt', 'TheAmericanFullBookSummarySparkNotes.txt', 'HarryPotterandtheSorcerer’sStoneFullBookSummarySparkNotes.txt', 'BleakHouseFullBookSummarySparkNotes.txt', 'HarryPotterandtheGobletofFireFullBookSummarySparkNotes.txt', 'OneFlewOvertheCuckoo’sNestFullBookSummarySparkNotes.txt', 'TheKitchenGodsWifeFullBookSummarySparkNotes.txt', 'BridesheadRevisitedFullBookSummarySparkNotes.txt', 'DivergentFullBookSummarySparkNotes.txt', 'DuneFullBookSummarySparkNotes.txt', 'Midnight’sChildrenFullBookSummarySparkNotes.txt', 'HarryPotterandtheDeathlyHallowsFullBookSummarySparkNotes.txt', 'DiaryofaWimpyKidFullBookSummarySparkNotes.txt', 'FlowersforAlgernonFullBookSummarySparkNotes.txt'})
+
+    def test_boolean_not(self):
+        self.assertEqual(self.inters.intersect_not(), {'HarryPotterandtheSorcerer’sStoneFullBookSummarySparkNotes.txt'})
