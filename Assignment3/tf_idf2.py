55813b3269b82c27d9be7be15a127faad4f81ff6
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-05-22 07:38:24 +0200

    modified both tf and idf

--- Assignment3/tf_idf2.py
+++ Assignment3/tf_idf2.py
@@ -3,38 +3,55 @@ import math
 
 
 class TF_IDF:
-    def __init__(self, total_document, index_dict, word_count):
+    def __init__(self, total_document, word_count):
         self.total_document = total_document
-        self.index_dict = index_dict
         self.word_count = word_count
 
-    def term_freq(self, document, word):
+    def term_freq(self, document, count_dict):
+        self.count_dict = count_dict
         self.document = document
-        self.word = word
+        term_freq_dict = {}
 
         N = len(self.document)
-        print(N)
-        occurrence = len([token for token in self.document if token == self.word])
-        return 1 + np.log10(occurrence/N)
+        for word in self.document:
+            term_freq_dict[word] = self.count_dict[word] / N
 
-    def idf(self, word):
-        self.word = word
-        try:
-            word_occurrence = self.word_count[self.word] + 1
-        except:
-            word_occurrence = 1
+        return term_freq_dict
 
-        return math.log10(self.total_document/word_occurrence)
+    def word_count_per_document(self, u_word_set, corpus_lists):
+        w_c_p_d_dict = {}
+        self.u_word_set = u_word_set
+        self.corpus_lists = corpus_lists
+        for word in self.u_word_set:
+            w_c_p_d_dict[word] = 0
+            for document in self.corpus_lists:
+                if word in document:
+                    w_c_p_d_dict[word] += 1
 
-    def tf_idf(self, sentence, word_set):
-        self.sentence = sentence
+        return w_c_p_d_dict
+
+    def idf(self, w_c_p_d_dict, n_documents):
+        self.w_c_p_d_dict = w_c_p_d_dict
+        self.n_documents = n_documents
+        idf_dict = {}
+
+        for word in self.w_c_p_d_dict:
+            idf_dict[word] = self.n_documents/self.w_c_p_d_dict[word]
+
+        return idf_dict
+
+    def tf_idf(self, document, word_set, term_freq_dict, idf_dict, idx_dict):
+        self.document = document
         self.word_set = word_set
+        self.term_freq_dict = term_freq_dict
+        self.idf_dict = idf_dict
+        self.idx_dict = idx_dict
         tf_idf_vec = np.zeros((len(self.word_set),))
-        for word in self.sentence:
-            tf = self.term_freq(self.sentence, word)
-            idf = self.idf(word)
+        for word in self.document:
+            tf = self.term_freq_dict[word]
+            idf = self.idf_dict[word]
 
-            value = tf*idf
-            tf_idf_vec[self.index_dict[word]] = value
+            weight = (1 + np.log(tf)) * (np.log10(idf))
+            tf_idf_vec[self.idx_dict[word]] = weight
 
         return tf_idf_vec
