e26ab463fa3b899b7b7af19144d5e8f34c10460e
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-05-22 07:39:00 +0200

    modified both index and tokenization

--- Assignment3/vocabulary.py
+++ Assignment3/vocabulary.py
@@ -7,40 +7,25 @@ class Vocabulary:
     def __init__(self, path):
         self.path = open(path, 'r')
 
-    def text_to_sent_tokenize(self):
-        sentences = []
-        lemmatizer = WordNetLemmatizer()
-        stop_words = set(stopwords.words('english'))
-        for sent in self.path:
-            x = [lemmatizer.lemmatize(i.lower()) for i in word_tokenize(sent) if i.isalpha() and i not in stop_words]
-            sentences.append(x)
-        return sentences
+    def text_tokenize(self):
+        token_list = []
+        # lemmatizer = WordNetLemmatizer()
+        # stop_words = set(stopwords.words('english'))
+        for line in self.path:
+            for token in line.split():
+                if token.isalpha():
+                    token_list.append(token.lower())
+        return token_list
 
-    def sent_tokenize_to_word_set(self, sentences):
-        self.sentences = sentences
-        word_set = []
-        for t_sent in self.sentences:
-            for word in t_sent:
-                if word not in word_set:
-                    word_set.append(word)
-        return word_set
+    def token_list_to_word_set(self, token_list):
+        self.token_list = token_list
+        return set(self.token_list)
 
-    def make_set(self, word_set):
+    def make_index(self, word_set):
         self.word_set = word_set
-
-        word_setted = set(self.word_set)
-        return word_setted
-
-    def number_of_doc(self, sentences):
-        self.sentences = sentences
-        number_of_doc = len(self.sentences)
-        return number_of_doc
-
-    def make_index(self, word_sett):
-        self.word_sett = word_sett
         index_dict = {}
         i = 0
-        for word in self.word_sett:
+        for word in self.word_set:
             index_dict[word] = i
             i += 1
         return index_dict
